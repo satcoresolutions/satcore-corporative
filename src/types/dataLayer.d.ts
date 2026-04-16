a76a22030f940440447f8a8bc2df2c4f377c2608
Erick Hernández Torres <erick.hdz9628@gmail.com>
2026-04-16 12:20:49 -0500

    CTA CSP

---
+++ src/types/dataLayer.d.ts
@@ -0,0 +1,41 @@
+// types/dataLayer.d.ts
+
+declare global {
+  interface BaseEvent {
+    event: string;
+  }
+
+  interface HeroCTAEvent extends BaseEvent {
+    event: "hero_cta_click";
+    cta_type: string;
+    cta_label: string;
+    section: string;
+    page_path: string;
+  }
+
+  interface CTAEvent extends BaseEvent {
+    event: "cta_click";
+    cta_type: string;
+    cta_label: string;
+    section: string;
+    page_path: string;
+  }
+
+  interface GenerateLeadEvent extends BaseEvent {
+    event: "generate_lead";
+    form_name: string;
+    page_path: string;
+  }
+
+  type DataLayerEvent =
+    | HeroCTAEvent
+    | CTAEvent
+    | GenerateLeadEvent
+    | BaseEvent;
+
+  interface Window {
+    dataLayer: DataLayerEvent[];
+  }
+}
+
+export {};
